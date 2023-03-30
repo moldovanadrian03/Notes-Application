7ed22f7958767d0ffd36b6db9b05e2886211b312
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-31 00:59:01 +0300

    Created the notes service and implemented the onSubmit method on the note-details-component.

--- notes-app/src/app/pages/note-details/note-details.component.ts
+++ notes-app/src/app/pages/note-details/note-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-note-details',
@@ -10,10 +11,15 @@ import { Note } from 'src/app/shared/note.model';
 export class NoteDetailsComponent implements OnInit{
   note: Note;
 
+  constructor(private notesService: NotesService) {}
+
   ngOnInit(): void {
       this.note = new Note();
   }
   onSubmit(form: NgForm) {
     console.log(form);
+
+    //save the notes
+    this.notesService.add(form.value);
   }
 }
