500ff9e3415c1ae38f5b497c2e05e36e2d76e4b2
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-30 23:58:55 +0300

    Added validation for title input and added onSubmit function.

--- notes-app/src/app/pages/note-details/note-details.component.ts
+++ notes-app/src/app/pages/note-details/note-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-note-details',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class NoteDetailsComponent {
 
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
