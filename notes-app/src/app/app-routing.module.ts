92d310a453aed7f9cf0ba7a35ea9c04a69fd1caf
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-30 14:54:29 +0300

    Created main-layout template and added nesting routing for injecting the notes-list component in main-layout component.

--- notes-app/src/app/app-routing.module.ts
+++ notes-app/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 const routes: Routes = [
-  { path: '', component: NotesListComponent }
+  { path: '', component: MainLayoutComponent, children:[
+    { path: '', component: NotesListComponent }
+  ] }
 ]
 
 @NgModule({
