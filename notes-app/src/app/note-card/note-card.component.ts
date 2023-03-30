141ca6d57001fa81a543149129b1808300316685
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-30 21:32:37 +0300

    Added functionality to truncator variable, we only use it if we have a text overflow on the NoteCard.

--- notes-app/src/app/note-card/note-card.component.ts
+++ notes-app/src/app/note-card/note-card.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
-export class NoteCardComponent {
+export class NoteCardComponent implements OnInit{
+  @ViewChild('truncator', {static: true}) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', {static: true}) bodyText: ElementRef<HTMLElement>;
 
+  constructor(private renderer: Renderer2) {
+
+  }
+
+  ngOnInit() {
+    //work out if there is a text overflow and if not, then hide the truncator
+
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = (style.getPropertyValue("height"), 72);
+
+    if(this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      //if there is no text overflow,  show  the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    }else {
+      //else (there is a text overFlow),  hide  the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
 }
