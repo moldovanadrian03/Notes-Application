e8376574e5b38669df094eea9703b011eaf3f09e
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-31 02:00:04 +0300

    Checking in the ngOnInit method if the note is new or not.

--- notes-app/src/app/pages/note-details/note-details.component.ts
+++ notes-app/src/app/pages/note-details/note-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 
@@ -11,11 +11,26 @@ import { NotesService } from 'src/app/shared/notes.service';
 })
 export class NoteDetailsComponent implements OnInit{
   note: Note;
+  noteId: number;
+  new: boolean;
 
-  constructor(private notesService: NotesService, private router: Router) {}
+  constructor(private notesService: NotesService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
       this.note = new Note();
+
+      //we want to find out if we are creating a new note or editing an existing one
+      this.route.params.subscribe((params: Params) => {
+        if(params['id']) {
+          this.note = this.notesService.get(params['id']);
+          this.noteId = params['id'];
+          this.new = false;
+        } else {
+          this.new = true;
+        }
+        console.log("note id is: ", this.noteId);
+        console.log("note new: ", this.new);
+      })
   }
   onSubmit(form: NgForm) {
     console.log(form);
