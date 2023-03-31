0fb07c891d814f5ed36b3d2938401441a39f173e
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-31 04:02:46 +0300

    Added animation for deleting a note.

--- notes-app/src/app/pages/notes-list/notes-list.component.ts
+++ notes-app/src/app/pages/notes-list/notes-list.component.ts
@@ -11,7 +11,7 @@ import { transition, trigger,style, animate } from '@angular/animations';
   animations: [
     trigger('itemAnim', [
       //ENTRY ANIMATION
-      transition('void => *', [
+      transition('void => *', [ //void transition to any state
         //INITIAL STATE
         style({
           height: 0,
@@ -35,6 +35,32 @@ import { transition, trigger,style, animate } from '@angular/animations';
           paddingRight: '*',
         })),
         animate(100)
+      ]),
+      transition('* => void',[ //any state transition to void
+        //first scale up
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        //then scale down to normal size while beginning to scale down
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        //scale down and fade out completely
+        animate('120ms ease-out', style({
+          transform: 'scale(0.65)',
+          opacity: 0
+        })),
+        //then animate the spacing (which includes the height margin and padding )
+        animate('150ms ease-out', style({
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          'margin-bottom': 0,
+          display: 'block',
+        }))
       ])
     ])
   ]
